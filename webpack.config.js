fb18fc2be584f4069f1e56fce54513ef6f3a115c
Sam Fentress <sfentress@concord.org>
2016-10-11 10:50:10 -0400

    First draft of photosynthesis model

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/code/app.js',
+  output: {
+    path: path.join(__dirname, 'public', 'js'),
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        }
+      },
+      {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      }
+    ]
+  }
+};
