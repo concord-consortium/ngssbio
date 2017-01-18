38c7737187e9640ed4c27f7a10f4a9902a9f9653
Noah Paessel <knowuh@gmail.com>
2017-01-17 22:01:47 -0500

    Finish model 6

--- model-6/model-6.js
+++ model-6/model-6.js
@@ -1,6 +1,7 @@
 (function() {
-  var run = 1;
+  var run = 1,
       rows = document.getElementsByTagName("tr"),
+      tableBody = document.getElementById("data")
       simulateButton = document.getElementById("simulate");
 
   function addClass(el, className) {
@@ -10,11 +11,24 @@
       el.className += ' ' + className;
   }
 
+  function addRow() {
+    var newRowMarkup = ''+
+      '<td><div class="inputs"></div></td>' +
+      '<td><div class="output"></div></td>' +
+      '<td><div class="output"></div></td>';
+    var rowElm = document.createElement("tr");
+    rowElm.innerHTML = newRowMarkup;
+    tableBody.appendChild(rowElm);
+    rowElm.scrollIntoView();
+  }
+
   function addInputImage(className) {
+    if(rows.length <= run+1) {
+      addRow();
+    }
     var row = rows[run],
         cell = row.querySelector(".inputs"),
         sunIcon = document.createElement("div");
-
     addClass(sunIcon, className);
     cell.appendChild(sunIcon);
   }
@@ -30,8 +44,6 @@
   }
 
   function simulate() {
-    if (run > 4) return;
-
     photosynthesisApp.updateOutputs();
     if (photosynthesisApp.co2High) {
       addInputImage("co2-img");
